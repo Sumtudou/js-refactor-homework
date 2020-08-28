3e402e029eb4868b609f1a4a998ed441275a23a4
Sumtudou <1163015195@qq.com>
2020-08-29 00:16:01 +0800

    [refactor][Jay] refactor: remove most if to use ?: and use "base" replace "magic number" in delivery.js

--- src/delivery.js
+++ src/delivery.js
@@ -20,26 +20,19 @@ const ME_NH = [
 
 function deliveryDate(anOrder, isRush) {
     if (isRush) {
-        let deliveryTime;
-        if (MA_CT.includes(anOrder.deliveryState)) {
-            deliveryTime = 1;
-        } else if (NY_NH.includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        } else {
-            deliveryTime = 3;
-        }
-        return anOrder.placedOn.plusDays(1 + deliveryTime);
+        let base = 1;
+        let deliveryTime = base;
+        deliveryTime += MA_CT.includes(anOrder.deliveryState) ? 1 : 0;
+        deliveryTime += NY_NH.includes(anOrder.deliveryState) ? 2 : 0;
+        deliveryTime += deliveryTime === base ? 3 : 0;
+        return anOrder.placedOn.plusDays(deliveryTime);
     } else {
-        let deliveryTime;
-        if (MA_CT_NY.includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        } else if (ME_NH.includes(anOrder.deliveryState)
-        ) {
-            deliveryTime = 3;
-        } else {
-            deliveryTime = 4;
-        }
-        return anOrder.placedOn.plusDays(2 + deliveryTime);
+        let base = 2;
+        let deliveryTime = base;
+        deliveryTime += MA_CT_NY.includes(anOrder.deliveryState) ? 2 : 0;
+        deliveryTime += ME_NH.includes(anOrder.deliveryState) ? 3 : 0;
+        deliveryTime += deliveryTime === base ? 4 : 0;
+        return anOrder.placedOn.plusDays(deliveryTime);
     }
 }
 
