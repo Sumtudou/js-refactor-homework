7575331970642c03aa71ed81d1dfef107c9a06f9
Sumtudou <1163015195@qq.com>
2020-08-29 01:02:47 +0800

    [refactor][Jay] refactor: Completely separate the data and formatted output, and extract several methods in print.js

--- src/print.js
+++ src/print.js
@@ -1,20 +1,38 @@
 function printOwing(invoice) {
-    let outstanding = 0;
-    console.log('***********************');
-    console.log('**** Customer Owes ****');
-    console.log('***********************');
+    return getFormatTextResult(getData(invoice));
+}
 
-    // calculate outstanding
-    for (const o of invoice.borderSpacing) {
-        outstanding += o.amount;
+function getData(invoice){
+    return  {
+        'customer':invoice.customer,
+        'amount':getOutStanding(invoice),
+        'date':getDateOf30DaysLater(),
     }
+}
 
-    // record due date
+function getFormatTextResult(data) {
+    result = "***********************\n" +
+        "**** Customer Owes ****\n" +
+        "***********************\n";
+    result += `name: ${data.customer}\n`;
+    result += `amount: ${data.amount}\n`;
+    result += `amount: ${data.date.toLocaleDateString()}\n`;
+    return result;
+}
+
+function getDateOf30DaysLater(){
     const today = new Date();
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
 
-    // print details
-    console.log(`name: ${invoice.customer}`);
-    console.log(`amount: ${outstanding}`);
-    console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+function getOutStanding(invoice) {
+    let outstanding = 0;
+    for (const borderSpacing of invoice.borderSpacing) {
+        outstanding += borderSpacing.amount;
+    }
+    return outstanding;
+}
+
+module.exports = {
+    printOwing
 }
